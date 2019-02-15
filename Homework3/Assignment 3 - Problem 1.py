0d66fcad429c1b74c3ca61ac6f5a6d608510ff98
Deryk <derahner@outlook.com>
2019-02-14 20:27:08 -0800

    Problem 1 Complete

--- Homework3/Assignment 3 - Problem 1.py
+++ Homework3/Assignment 3 - Problem 1.py
@@ -1,6 +1,6 @@
 """
 ASSIGNMENT 3 - PROBLEM 1
-OBJECTIVE: Determine is a tank will overflow and drain
+OBJECTIVE: Determine if (and when) a tank will overflow or drain
 FLUID - Water [assuming incompressible]
 """
 
@@ -36,13 +36,36 @@ Mdot_o = rho*A_o*V_i            #Mdot coming out of the tank [kg/s]
 
 Mdot_sys = Mdot_i-Mdot_o        #Mdot of the system [kg/s]
 
-if Mdot_sys > 0:                        #Logic to determine system state
+#Determing when tank will fill or drain
+Vol_tot = ((np.pi/4)*D_t**2)*H_t         #Finding the total volume of the tank [m^3]
+Vol_i = ((np.pi/4)*D_t**2)*H_o           #Finding the total volume of the tank [m^3]
+m_tot = rho*Vol_tot                      #Finding the total mass of a filled tank [kg]
+m_i = rho*Vol_i                          #Finding the initial mass of the water in the tank [kg]
+
+if Mdot_sys > 0:                        #Logic to determine system state and time determination
     print("The tank level is rising")
+    mass = m_tot-m_i
+    t = mass/Mdot_sys
+    print(t,"seconds")
 elif Mdot_sys < 0:
     print("The tank level is lowering")
+    mass = m_i
+    t = abs(mass/Mdot_sys)
+    #This needs verification for a system that is lowering
 else:
     print("The tank level constant")
 
-#Determing when tank will fill or drain
-Vol_tot = ((np.pi/4)*D_t**2)*H_t         #Finding the total volume of the tank [m^3]
-Vol_i = ((np.pi/4)*D_t**2)*H_t           #Finding the total volume of the tank [m^3]
+'''TIME BASED VERIFICATION'''
+time = np.linspace(0,t+50,1001)     #Linear space array for time
+m_cur = m_i+(Mdot_sys*time)         #Current mass of tank [kg]
+vol_cur = m_cur/rho                 #Current volume of water [m^3]
+
+plt.plot(time,vol_cur)              #Plotting current volume of tank
+plt.plot([0,t+50],[Vol_tot,Vol_tot])#Plotting line of max volume
+plt.plot(t,Vol_tot,'*')             #Plotting point of fill
+plt.xlabel("Time [seconds]")
+plt.ylabel("Volume [m^3]")
+plt.title("Volume of tank vs. time")
+plt.grid()
+plt.legend(('Current Tank Volume','Max Tank Volume','Calculated Fill Point'))
+plt.show()
