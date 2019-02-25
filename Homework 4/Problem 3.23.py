c85fa2ac51eae0f6edb352143aa70324100f29b6
Deryk <derahner@outlook.com>
2019-02-24 17:12:45 -0800

    Problem 3.23 a,b complete

--- Homework 4/Problem 3.23.py
+++ Homework 4/Problem 3.23.py
@@ -26,27 +26,57 @@ T_2 = PropsSI('T','P',float(pressure.item(2)),'H',h_2,fluid.item(2))            
 Tavg_water = (float(temp.item(5))+float(temp.item(6)))/2
 Tavg_air = (T_2+T_3)/2
 
-Cp_water = PropsSI('C','T',Tavg_water,'P',float(pressure.item(5)),fluid.item(5))      #C_p for water assuming average temp and atm pressure
+Cp_water = PropsSI('C','T',float(temp.item(5)),'P',float(pressure.item(5)),fluid.item(5))      #C_p for water assuming average temp and atm pressure
 Cp_air = PropsSI('C','T',Tavg_air,'P',float(pressure.item(3)),fluid.item(3))          #C_p for air assuming average temp
 
 #Determining density
-rho_air = PropsSI('D','T',Tavg_air,'P',float(pressure.item(3)),fluid.item(3))
+rho_air = PropsSI('D','T',float(temp.item(1)),'P',float(pressure.item(1)),fluid.item(1))
+
+#Enthalpies for part 2
+h_5 = PropsSI('H','T',float(temp.item(5)),'P',float(pressure.item(5)),fluid.item(5))
+h_6 = PropsSI('H','T',float(temp.item(6)),'P',float(pressure.item(6)),fluid.item(6))
+h_3 = PropsSI('H','T',T_3,'P',float(pressure.item(3)),fluid.item(3))
+s_3 = PropsSI('S','T',T_3,'P',float(pressure.item(3)),fluid.item(3))
+h_4_s = PropsSI('H','P',float(pressure.item(4)),'S',s_3,fluid.item(4))
+h_4 = ((h_4_s-h_3)/n_c)+h_3                                                     #Enthalpy at state 4
+T_4 = PropsSI('T','H',h_4,'P',float(pressure.item(4)),fluid.item(4))
 
 """STATE TABLE ADDITIONS"""
-h = np.array(["ENTHALPY",h_1,h_2,"unknown","unknown","unknown","unknown"])
-temp = np.array(["TEMP[K]",25+273.15,T_2,T_3,"unknown",25+273.15,75+273.15])
+h = np.array(["ENTHALPY",h_1,h_2,h_3,h_4,h_5,h_6])
+temp = np.array(["TEMP[K]",25+273.15,T_2,T_3,T_4,25+273.15,75+273.15])
 
 """SOLUTION A"""
 mdot_air = Vdot_air*rho_air
 Cdot_air = mdot_air*Cp_air
 num = Cdot_air*(float(temp.item(2))-float(temp.item(3)))
-print(float(temp.item(5)))
-print(float(temp.item(6)))
 dem = float(temp.item(5))-float(temp.item(6))
 Cdot_water = num/dem
-mdot_water = Cdot_water/Cp_water
+mdot_water = abs(Cdot_water/Cp_water)
+rho_water_5 = PropsSI('D','T',float(temp.item(5)),'P',float(pressure.item(5)),fluid.item(5))
+vdot_water = mdot_air/rho_water_5
+vdot_water = vdot_water*15850.372483753                                                     #Vdot water in GPM
 
-print(mdot_water,"[kg/s]")
+"""SOLUTION B"""
 
+in_terms = (mdot_air*float(h.item(1)))+(mdot_water*float(h.item(5)))
+out_terms = (mdot_air*float(h.item(4)))+(mdot_water*float(h.item(6)))
+Wdot_C = out_terms-in_terms
+Wdot_C = Wdot_C*(1/1000)
 
+"""SOLUTION PRINTING"""
+print("Solution A - v_dot_water =",vdot_water,"[gpm]")
+print("Solution B - Wdot_C = ",Wdot_C,"kW")
 
+"""STATE TABLE"""
+master_table = np.vstack((points,fluid,temp,pressure,h))
+print("STATE TABLE")
+print(master_table)
+
+"""TROUBLEHSOOTING"""
+print(" ")
+print("Troubleshooting")
+print("Mdot Air",mdot_air)
+print("Rho Air",rho_air)
+print("Isentripic enthalpy",h_2_s)
+print("Enthalpy at 2",h_2)
+print("Mdot Water",mdot_water,"[kg/s]")
