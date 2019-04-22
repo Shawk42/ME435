ac97968fab2cb5cb54a98b9317a58061ce4bdbc1
Deryk <derahner@outlook.com>
2019-04-21 18:16:57 -0700

    Quiz 9 Added, EC2 Started

---
+++ Extra Credit 2/4.43.py
@@ -0,0 +1,70 @@
+print("Problem 4.43")
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""GIVENS"""
+V_dot = 400     #[gpm] 400 gpm required
+#V_dot = np.linspace(400,600,1001)
+Delta_Z = 30    #[ft]
+L = 85          #[ft]
+
+"""TABLE VALUES"""
+T = 55                  #[F]
+D = 0.42058             #[ft]
+rho = 0.3611            #[lbm/in^3]
+mu = 6.768*(10**-5)     #[lbm/in-s]
+epsilon = 0.00015       #[ft]
+g = 32.2                #[ft/s^2]
+
+
+"""REYNOLDS NUMBER"""
+D_in = D*12                 #[in] Diamter of pipe                   - Correct Conversion
+V_dot_ft = V_dot*0.002228   #[ft^3/s] flow rate of water            - Correct Conversion
+V_dot_in = V_dot_ft*1728    #[in^3/s] flow rate of water
+Area_in = np.pi*(D_in/2)**2 #[in^2] cross sectional area of pipe
+V_in = V_dot_in*Area_in     #[in/s] velocity of water
+V = V_in*(1/12)             #[ft/s] velocity of water
+
+Re = (rho*V_in*D_in)/mu
+
+#Section Troubleshooting
+print("Pipe Diamter",D_in,"in")
+print("V_dot_ft",V_dot_ft,"ft^3/s")
+print("V_dot_in",V_dot_in,"in^3/s")
+print("Area",Area_in,"in^2")
+print("Velocity inches",V_in,"in/s")
+print("Velcoity feet",V,"ft/s")
+
+
+"""FRICTION FACTOR"""
+#Fluid is always turbulent
+
+epD = epsilon/D  #epsilon/D
+logdem = (epD/3.7)+(5.74/(Re**0.9))
+dem = np.log10(logdem)**2
+
+f = 0.25/dem
+
+"""K Losses"""
+#Entrance Losses
+K_1 = 1000
+K_inf = 0.5
+K_ent = (K_1/Re)+K_inf*(1+(1/D))
+#Elbow Losses - assuming threaded
+K_1 = 800
+K_inf = 0.4
+K_e = (K_1/Re)+K_inf*(1+(1/D))
+K_e = 3*K_e
+#Exit Losses
+K_1 = 800
+K_inf = 0.4
+K_ext = (K_1/Re)+K_inf*(1+(1/D))
+
+
+
+Hp = Delta_Z+(f*(L/D)+K_ent+3*K_e+K_ext)*(V**2/(2*g))
+
+
+quit()
+plt.plot(V_dot,Hp)
+plt.show()
--- Quiz9.py
+++ Quiz9.py
@@ -1,16 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""COMMON NUMBERS"""
+#Cold Fluid
+t_o = 80
+t_i = 30
+#Hot Fluid
+T_i = 98
+T_o = np.linspace(30,T_i-0.000001,1000)
+
+
+"""SOLUTION"""
 R = (T_i-T_o)/(t_o-t_i)
 P = (t_o-t_i)/(T_i-t_i)
 
-
 F_num = np.sqrt(R**2+1)*np.log((1-P)/(1-P*R))
-
 F_lognum = 2-P*(R+1-np.sqrt(R**2+1))
 F_logdem = 2-P*(R+1+np.sqrt(R**2+1))
 F_log = F_lognum/F_logdem
-
 F_dem = (R-1)*np.log(F_log)
 
 F = F_num/F_dem
+
+"""PLOTTING"""
+T_i_delta = T_i-T_o
+
+plt.subplot(2,1,1)
+plt.plot(T_i_delta,F)
+plt.grid()
+plt.xlabel("Delta T Hot Fluid [C]")
+plt.ylabel("F")
+plt.title("Evaporator F curve - Deryk Ahner - Quiz 9")
+
+plt.subplot(2,1,2)
+plt.text(-0.125,0.125,"As shown in the above graph F approaches 1 in a evaporator or condenser. In both of these devices"
+               " the inlet and outlet temperatures of the working fluid are relatively constant (due to the phase change)"
+               " To determine the behavior of F, the outlet temperature was varied (in a linear spaced array) from"
+               " the cold fluid inlet temperature to the hot fluid inlet temperature. To better show the F and temperature"
+               " relationship the x-axis is the difference between the hot fluid inlet and outlet temperature. Therefore"
+               " according the graph F approaches 1 in a phase change head exhanger, as the difference between inlet and "
+               " outlet temperature goes to zero. The relationship also holds true for a condensor, however the graph "
+                " has a positive second derivative (upward facing) ", wrap=True)
+plt.axis('off')
+
+plt.subplots_adjust(hspace = 0.5)
+plt.show()
